a78a2d6bbef0735cae95ab40a094a91839a2adaa
mattbzca <111474919+mattbzca@users.noreply.github.com>
2023-02-25 22:39:48 -0800

    Add project files.

---
+++ COMP003A.Assignment6/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Assignment6
+{
+    /// <summary>
+    /// Car extends the Vehicle class
+    /// </summary>
+    internal class Car : Vehicle
+    {
+        /***** constructors section *****/
+        // by default, a class has a default constructor that does not take any parameters
+        // therfore, you do not have to explicitly create one 
+
+        /***** methods section *****/
+        /// <summary>
+        /// Overrides the base Vehicle's definition for GetInfo()
+        /// </summary>
+        public override void GetInfo()
+        {
+            Console.WriteLine($"I'm a {nameof(Car)}");
+            Console.WriteLine("I don't want to disclose my information.");
+        }
+    }
+}
---
+++ COMP003A.Assignment6/IVehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Assignment6
+{
+    /// <summary>
+    /// Interfaces are contracts that the inheriting class has to implement.
+    /// Read https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/abstractions-abstract-types-and-interfaces
+    /// </summary>
+    internal interface IVehicle
+    {
+        /// <summary>
+        /// this method is required to be implemented in the inheriting class
+        /// </summary>
+        public void GetInfo();
+    }
+}
